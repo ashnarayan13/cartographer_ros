c317d6c5c43b428c4df5c95d949c8ec92e9a6746
Ashwath Narayan Murali <ash.narayan13@gmail.com>
2019-04-04 22:20:48 +0200

    Added tuning params

--- cartographer_ros/configuration_files/vlp32.lua
+++ cartographer_ros/configuration_files/vlp32.lua
@@ -44,14 +44,24 @@ options = {
 }
 
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+MAX_3D_RANGE = 150 --Maximum range to use from LiDAR
+TRAJECTORY_BUILDER_3D.min_range = 2 --Minimum range to use from LiDAR
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 50 --Num of scans before a submap is forced to be created
+
+--Handling weights for LOCAL SLAM
+TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = false --use fast correlative scan matcher
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 5 --Default is 5
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 4e2 --Default 4e2
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = true --Default is false
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
-POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 50
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.optimization_problem.huber_scale = 5e2 --Higher implies higher weight of outliers
+POSE_GRAPH.optimize_every_n_nodes = 0 --Set to 0 to disable global SLAM
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.1 --Number of close nodes to be sampled for LC original(0.03)
 POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 POSE_GRAPH.constraint_builder.min_score = 0.62
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+POSE_GRAPH.optimization_problem.log_solver_summary = true --To log the IMU / tracking frame
 
 return options
